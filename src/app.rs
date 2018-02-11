d85d63300b4bd3824e45cd919814bfc0f2ea7750
Aqrun <aqruna@gmail.com>
2018-02-11 18:47:20 +0800

    modify

--- src/app.rs
+++ src/app.rs
@@ -1,49 +1,69 @@
 use fetch_access_token;
 use get_env_url;
 
+// #[derive(Default)]
+// struct BaseUrl(Option<String>);
+
+// impl BaseUrl {
+//     fn get(&mut self) -> &str {
+//         self.0.get_or_insert_with(|| get_env_url())
+//     }
+// }
+
+// #[derive(Default)]
+// struct AccessToken(Option<String>);
+
+// impl AccessToken {
+//     fn get(&mut self, base_url: &str) -> &str {
+//         self.0.get_or_insert_with(|| fetch_access_token(base_url))
+//     }
+// }
+
+#[derive(Default)]
+struct Cache<T>(Option<T>);
+
+impl<T> Cache<T> {
+    fn get<F>(&mut self, f: F) -> &T
+    where 
+        F: FnOnce() -> T,
+    {
+        self.0.get_or_insert_with(f)
+    }
+}
+
+#[derive(Default)]
 pub struct App{
-    pub base_url: Option<String>,
-    pub access_token: Option<String>,
+    base_url: Cache<String>,
+    access_token: Cache<String>,
 }
 
 impl App {
     pub fn new() -> App {
-        let mut app = App{
-            base_url: None,
-            access_token: None,
-        };
-        app.init();
-        app
-    }
-    pub fn init(&mut self) {
-        let base_url = self.get_base_url().unwrap();
-    }
-    pub fn get_base_url(&mut self) -> Result<&str, ()> {
-        if self.base_url.is_none() {
-            self.base_url = Some(get_env_url());
-        }
-        Ok(self.base_url.as_ref().unwrap())
+        App::default()
     }
     pub fn get_access_token(&mut self) -> Result<&str, ()> {
-        if self.access_token.is_none() {
-            let base_url = self.base_url.as_ref().unwrap();
-            self.access_token = Some(fetch_access_token(base_url));
-        }
-        Ok(self.access_token.as_ref().unwrap())
+        let base_url = self.base_url.get(get_env_url);
+        let token = self.access_token.get(||fetch_access_token(base_url));
+        Ok(token)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::App;
+    use dotenv::dotenv;
 
-    lazy_static! {
-        static ref APP: App = App::new();
-    }
+    // lazy_static! {
+    //     static ref APP: App = App::new();
+    // }
 
     #[test]
     fn app_get_access_token_is_ok() {
-        let token = APP.get_access_token();
+        dotenv().ok();
+
+        let mut app = App::new();
+        let token = app.get_access_token();
+        //println!("=========== {}",token.unwrap());
         assert!(token.is_ok());
     }
 }
